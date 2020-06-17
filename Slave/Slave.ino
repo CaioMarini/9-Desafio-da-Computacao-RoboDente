45abadd53be84619e9a6879377bb958ee4bc8047
Caio Ferraz Marini <38476820+CaioMarini@users.noreply.github.com>
2020-06-16 21:24:35 -0300

    Update Slave.ino

--- Slave/Slave.ino
+++ Slave/Slave.ino
@@ -1,122 +1,180 @@
-/*
-   Arduino Robot Car Wireless Control using the HC-05 Bluetooth
-             == SLAVE DEVICE - Arduino robot car ==
-   by Dejan Nedelkovski, www.HowToMechatronics.com
-*/
+
 #define enA 9
 #define in1 4
 #define in2 5
 #define enB 10
 #define in3 6
 #define in4 7
-int xAxis, yAxis;
-unsigned int  x = 0;
-unsigned int  y = 0;
-const char  led = 0;
+const int led = 53;
+char recebido;
+int counter = 0;
+
+//Sensor Ultrassonico
+int PinTrigger = 22; //pino usado para disparar os pulsos do sensor
+int PinEcho = 24;//pino usado para ler a saida do sensor
+float TempoEcho =0;
+const float VelocidadeSom_mpors = 340;//em metros por segundo
+const float VelocidadeSom_mporus = 0.000340; // em metros por microsegundo
+
+
+//Carro
+
+
+
 int motorSpeedA = 0;
 int motorSpeedB = 0;
 
-const int ledpin = 53;
-
 
 void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(38400);
+  pinMode(led,OUTPUT);
+  //pinMode(buzzerPin,OUTPUT);
+
+  //CARRO
   pinMode(enA, OUTPUT);
   pinMode(enB, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
-  Serial.begin(38400); // Default communication rate of the Bluetooth module
 
-  pinMode(ledpin, OUTPUT);
-}
-void loop() {
-
+  pinMode(PinTrigger, OUTPUT);
+  digitalWrite(PinTrigger, LOW);
+  pinMode(PinEcho,INPUT);
   
-  // Default value - no movement when the Joystick stays in the center
-  x = 510 / 4;
-  y = 510 / 4;
-  // Read the incoming data from the Joystick, or the master Bluetooth device
-  while (Serial.available() >= 2) {
-    x = Serial.read();
-    delay(10);
-    y = Serial.read();
-    delay(10);
-    led = Serial.read();
+  delay(100);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  DisparaPulsoUltrassonico();
+  
+ TempoEcho = pulseIn(PinEcho, HIGH);
+
+
+  if(Serial.available() > 0){
+    recebido = Serial.read();
   }
-  delay(10);
-  // Convert back the 0 - 255 range to 0 - 1023, suitable for motor control code below
-  xAxis = x*4;
-  yAxis = y*4;
-  // Y-axis used for forward and backward control
-  if (yAxis < 470) {
-    // Set Motor A backward
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    // Set Motor B backward
+/*
+  if(recebido == 'F'){
+   //Colocar motor A para RÈ
+     digitalWrite(in1, HIGH);
+     digitalWrite(in2, LOW);
+    //Colocar motor B para RE 
     digitalWrite(in3, HIGH);
     digitalWrite(in4, LOW);
-    // Convert the declining Y-axis readings for going backward from 470 to 0 into 0 to 255 value for the PWM signal for increasing the motor speed
-    motorSpeedA = map(yAxis, 470, 0, 0, 255);
-    motorSpeedB = map(yAxis, 470, 0, 0, 255);
-  }
-  else if (yAxis > 550) {
+    motorSpeedA = 255;
+    motorSpeedB = 255;
+    
+  }else if (recebido == 'T'){
     // Set Motor A forward
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
     // Set Motor B forward
     digitalWrite(in3, LOW);
     digitalWrite(in4, HIGH);
-    // Convert the increasing Y-axis readings for going forward from 550 to 1023 into 0 to 255 value for the PWM signal for increasing the motor speed
-    motorSpeedA = map(yAxis, 550, 1023, 0, 255);
-    motorSpeedB = map(yAxis, 550, 1023, 0, 255);
-  }
-  // If joystick stays in middle the motors are not moving
-  else {
-    motorSpeedA = 0;
-    motorSpeedB = 0;
-  }
-  // X-axis used for left and right control
-  if (xAxis < 470) {
-    // Convert the declining X-axis readings from 470 to 0 into increasing 0 to 255 value
-    int xMapped = map(xAxis, 470, 0, 0, 255);
-    // Move to left - decrease left motor speed, increase right motor speed
-    motorSpeedA = motorSpeedA - xMapped;
-    motorSpeedB = motorSpeedB + xMapped;
-    // Confine the range from 0 to 255
-    if (motorSpeedA < 0) {
+    motorSpeedA = 255;
+    motorSpeedB = 255;  
+    
+  }else if(recebido == 'D'){
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
       motorSpeedA = 0;
-    }
-    if (motorSpeedB > 255) {
-      motorSpeedB = 255;
-    }
-  }
-  if (xAxis > 550) {
-    // Convert the increasing X-axis readings from 550 to 1023 into 0 to 255 value
-    int xMapped = map(xAxis, 550, 1023, 0, 255);
-    // Move right - decrease right motor speed, increase left motor speed
-    motorSpeedA = motorSpeedA + xMapped;
-    motorSpeedB = motorSpeedB - xMapped;
-    // Confine the range from 0 to 255
-    if (motorSpeedA > 255) {
+    motorSpeedB = 255;
+  }else if(recebido == 'E'){
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
       motorSpeedA = 255;
-    }
-    if (motorSpeedB < 0) {
-      motorSpeedB = 0;
-    }
-  }
-  // Prevent buzzing at low speeds (Adjust according to your motors. My motors couldn't start moving if PWM value was below value of 70)
-  if (motorSpeedA < 70) {
-    motorSpeedA = 0;
-  }
-  if (motorSpeedB < 70) {
+    motorSpeedB = 0;
+  }else if(recebido == 'A'){
+     motorSpeedA = 0;
     motorSpeedB = 0;
   }
+  */
+
+if((CalculaDistancia(TempoEcho)*100) >= 20){
+  if(recebido == 'F'){
+   frente();   
+  }else if (recebido == 'T'){
+    tras();   
+  }else if(recebido == 'D'){
+    direita();
+  }else if(recebido == 'E'){
+    esquerda();
+  }else if(recebido == 'A'){
+     motorSpeedA = 0;
+     motorSpeedB = 0;
+  }
+}else{
+   motorSpeedA = 0;
+     motorSpeedB = 0;
+}
+  
   analogWrite(enA, motorSpeedA); // Send PWM signal to motor A
   analogWrite(enB, motorSpeedB); // Send PWM signal to motor B
 
-  if(led = "l"){
-    digitalWrite(ledpin,HIGH);
+
+ 
+/*
+  if(recebido == 'F'){   
+    liga();
+  }else if(recebido == 'T'){
+    digitalWrite(led,LOW);
+    Serial.print("Ola");
+  }else if(recebido = 'a'){
+    digitalWrite(led,LOW);
   }
-  
+*/
+
+
+}//fim loop
+
+
+void DisparaPulsoUltrassonico(){
+  digitalWrite(PinTrigger,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(PinTrigger, LOW);
+}
+
+float CalculaDistancia(float tempo_us){
+  return ((tempo_us*VelocidadeSom_mporus)/2);
+}
+
+
+
+
+
+void frente(){
+//Colocar motor A para RÈ
+     digitalWrite(in1, HIGH);
+     digitalWrite(in2, LOW);
+    //Colocar motor B para RE 
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    motorSpeedA = 255;
+    motorSpeedB = 255;  
+}
+void tras(){
+  // Set Motor A forward
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    // Set Motor B forward
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    motorSpeedA = 255;
+    motorSpeedB = 255;  
+}
+void direita(){
+  digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+      motorSpeedA = 0;
+    motorSpeedB = 255;
+}
+void esquerda(){
+  digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+      motorSpeedA = 255;
+    motorSpeedB = 0;
 }
