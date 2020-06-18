f035f911ec20cc9fcd20b7260ddd85ff1b827189
Caio Ferraz Marini <38476820+CaioMarini@users.noreply.github.com>
2020-06-18 19:15:54 -0300

    Update Master.ino

--- Master/Master.ino
+++ Master/Master.ino
@@ -1,13 +1,18 @@
 
+
 #define pinVRx A0
 #define pinVRy A1
-
-
+#define button 7//Botao musica
+int buttonState = 0;//botao musica
+#define botopassum 8 //botao motor passo um
+int bpsum = 0;//botao passo um
+#define botopassdois 9//botao motor passi dois
+int bpsdois = 0;//botao passo dois
 void setup() {
   // put your setup code here, to run once:
   pinMode(pinVRx, INPUT);
   pinMode(pinVRy, INPUT);
-
+  pinMode(button,INPUT);
   Serial.begin(38400);
 
 }
@@ -17,12 +22,19 @@ void loop() {
   int valorVRx = analogRead(pinVRx);
   int valorVRy = analogRead(pinVRy);
 
+  buttonState = digitalRead(button);
+  bpsum = digitalRead(botopassum);
+  bpsdois = digitalRead(botopassdois);
+
   //Serial.print("\n   Valor VRX: ");
   //Serial.print(valorVRx);
   //Serial.print("   Valor VRy: ");
   //Serial.print(valorVRy);
 
-  if(valorVRx <= 50){
+  
+
+
+  if(valorVRx <= 300){
     //Serial.print("\nF");
     Serial.write('F');//Frente
     delay(20);
@@ -38,6 +50,16 @@ void loop() {
    // Serial.print("\nE");//esquerda
     Serial.write('E');
     delay(20);
+  }
+  else if(buttonState == HIGH){
+    Serial.write('O');
+    delay(300);
+  }else if(bpsum == HIGH){
+    Serial.write('W');
+    delay(300);
+  }else if(bpsdois == HIGH){
+    Serial.write('S');
+    delay(300);
   }else{
    Serial.write('A');
     delay(20);
